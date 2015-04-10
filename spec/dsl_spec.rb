e6111e49d714ae181281184cd4ef0b887e3a29a6
Sebastian Oelke <dev@sohleeatsworld.de>
2015-04-10 12:47:11 +0200

    Added more specs for reading DSL definition from file.

--- spec/dsl_spec.rb
+++ spec/dsl_spec.rb
@@ -34,8 +34,76 @@ RSpec.describe GarageInventory::Dsl do
     let(:dsl)     { described_class.new }
     let(:options) { { name: 'my_garage', size: 10, capacity: 3 } }
 
-    it_behaves_like 'a garage adder' do
-      subject { dsl.initialize_with_file(file_path) }
+    subject { dsl.initialize_with_file(file_path) }
+
+    it_behaves_like 'a garage adder'
+
+    context "the garage" do
+
+      let(:garage) { dsl.garages.first }
+
+      it "has two rooms" do
+        subject
+
+        expect(garage.rooms.size).to eq(2)
+      end
+
+      context "the first room" do
+
+        let(:room)             { garage.rooms.first }
+        let(:expected_options) { { name: 'truck hall', size: 150 } }
+
+        it "has the expected options set" do
+          subject
+
+          expect({ name: room.name, size: room.size }).to eq(expected_options)
+        end
+
+        it "has a screwdriver" do
+          subject
+
+          expect(room.tools[0].to_h).to eq({ name: 'screwdriver', size: 16, type: :phillips })
+        end
+
+        it "has a car lift" do
+          subject
+
+          expect(room.tools[1].to_h).to eq({ name: 'car_lift', lifting_capacity: 5000 })
+        end
+
+        it "has a brake test bench" do
+          subject
+
+          expect(room.tools[2].to_h).to eq({ name: 'brake_test_bench' })
+        end
+
+      end
+
+      context "the second room" do
+
+        let(:room)             { garage.rooms[1] }
+        let(:expected_options) { { name: 'car hall', size: 100 } }
+
+        it "has the expected options set" do
+          subject
+
+          expect({ name: room.name, size: room.size }).to eq(expected_options)
+        end
+
+        it "has an AC service" do
+          subject
+
+          expect(room.tools[0].to_h).to eq({ name: 'ac_service' })
+        end
+
+        it "has a balancing machine" do
+          subject
+
+          expect(room.tools[1].to_h).to eq({ name: 'balancing_machine', max_tire_size: 50 })
+        end
+
+      end
+
     end
 
   end
